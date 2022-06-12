2bfab688fe55f8ff91af16b5f32073d89633a8e1
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-06-12 11:20:25 +0300

    ft: create signup form

--- project/views.py
+++ project/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def create_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        user = User.objects.create_user(username, email, password)
+        user.save()
+        return render(request, 'index.html')
+    return render(request, 'authentication/signup.html')
