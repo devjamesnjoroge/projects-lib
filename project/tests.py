8efdf5eb9f3e5fa7141baaf4c075bcc21c76e3f6
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-06-15 08:26:09 +0300

    ft: add test cases for profile creation

--- project/tests.py
+++ project/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+
+class ProfileTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.profile = Profile.objects.create(user=self.user, bio='test bio', contact='test contact')
+
+    def test_profile_creation(self):
+        self.assertEqual(self.profile.bio, 'test bio')
+        self.assertEqual(self.profile.contact, 'test contact')
+        self.assertEqual(self.profile.user.username, 'testuser')
+
+    def test_profile_str(self):
+        self.assertEqual(self.profile.__str__(), 'testuser')
+
+    
