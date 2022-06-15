e2754aa1a79bd0f4a87cd1949360e0543657c845
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-06-15 08:27:08 +0300

    ft: add test cases for project creation

--- project/tests.py
+++ project/tests.py
@@ -16,4 +16,16 @@ class ProfileTest(TestCase):
     def test_profile_str(self):
         self.assertEqual(self.profile.__str__(), 'testuser')
 
+class ProjectTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.project = Project.objects.create(title='test title', description='test description', user=self.user)
+
+    def test_project_creation(self):
+        self.assertEqual(self.project.title, 'test title')
+        self.assertEqual(self.project.description, 'test description')
+        self.assertEqual(self.project.user.username, 'testuser')
+    
+    def test_project_str(self):
+        self.assertEqual(self.project.__str__(), 'test title')
     
